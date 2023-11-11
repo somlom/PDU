898cdbcaeaeaf6d6daf8d823f30c7c59f2c82be6
la-leche <lechenkodmitrii@gmail.com>
2023-11-11 11:42:01 +0100

    removed twind

--- server/routes/_app.tsx
+++ server/routes/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from "$fresh/server.ts";
+import "npm:bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "npm:react-bootstrap";
 
 export default function App({ Component }: AppProps) {
   return (
@@ -7,9 +9,11 @@ export default function App({ Component }: AppProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>server</title>
+        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
         <Component />
+        <Button />
       </body>
     </html>
   );
