16f37b98b3f8d98078878c0befd4a3beb197b108
Dmitrii Lechenko <lechenkodmitrii@gmail.com>
2023-12-09 10:30:16 +0100

    Simple sketch for test

--- firmware/sketch/sketch.ino
+++ firmware/sketch/sketch.ino
@@ -1,9 +1,9 @@
 #include <LiquidCrystal_I2C.h>
 
-const int SOCKETS[4] = { 18, 19, 20, 21 };
+const int SOCKETS[4] = { 16, 17, 18, 19 };
 int DOWN[4] = {};
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
-
+int uptime = 0;
 uint8_t ON[10] = {
   0b11111,
   0b11111,
@@ -14,7 +14,7 @@ uint8_t ON[10] = {
   0b11111,
   0b11111,
 };
-uint8_t OFF[10]  = {
+uint8_t OFF[10] = {
   0b11111,
   0b10001,
   0b10001,
@@ -31,16 +31,18 @@ void setup() {
   lcd.createChar(2, OFF);
   lcd.backlight();
   lcd.setCursor(0, 0);
-  
+  // lcd.print("hi");
+  // delay(10000);
   for (int i = 0; i < 4; i++) {
     pinMode(SOCKETS[i], OUTPUT);
-    digitalWrite(SOCKETS[i], HIGH);
+    digitalWrite(SOCKETS[i], LOW);
     lcd.print("\x02");
   }
+  digitalWrite(SOCKETS[0], HIGH);
   delay(2000);
 }
 
-void error(String message){
+void error(String message) {
   lcd.clear();
 
   lcd.setCursor(0, 0);
@@ -48,14 +50,6 @@ void error(String message){
 
   lcd.setCursor(0, 1);
   lcd.print(message);
-  
-  for (int i = 0; i < 2; i++) {
-    lcd.noBacklight();
-    delay(500);
-    lcd.backlight();
-    delay(500);
-  }
-  lcd.clear();
 }
 
 void loop() {
@@ -63,11 +57,13 @@ void loop() {
   for (int i = 0; i < 4; i++) {
     if (digitalRead(SOCKETS[i]) == LOW) {
       digitalWrite(SOCKETS[i], HIGH);
-      lcd.print("\x01");
+      // lcd.print("\x01");
     } else {
       digitalWrite(SOCKETS[i], LOW);
-      lcd.print("\x02");
+      // lcd.print("\x02");
     }
   }
-  delay(1000); // this speeds up the simulation
+  lcd.printf("Uptime: %d sec", uptime);
+  uptime++;
+  delay(1000);  // this speeds up the simulation
 }
